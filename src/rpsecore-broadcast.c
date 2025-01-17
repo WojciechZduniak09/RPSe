7cb70eb34dd84431856527e2cd5b28d55a6475c1
WojciechZduniak09 <151409152+WojciechZduniak09@users.noreply.github.com>
2025-01-17 21:55:45 +0100

    Receiver fixes ig?

--- src/rpsecore-broadcast.c
+++ src/rpsecore-broadcast.c
@@ -547,10 +547,19 @@ rpse_broadcast_receiveBroadcast(const broadcast_data_t *BROADCAST_DATA)
             crypto_stream_chacha20_xor((unsigned char *)current_node->data, (const unsigned char *)current_broadcast_data->encrypted_message, 
                                                                 strlen(current_broadcast_data->encrypted_message) + 1,
                                                                 (const unsigned char *)current_broadcast_data->nonce, (const unsigned char *)BROADCAST_CHACHA20_ENCRYPTION_KEY);
-	    while (head != NULL && head->data != NULL && head->data[strlen(head->data) - 1] != ')' && \
-		   !isdigit(head->data[strlen(head->data) -1]) && head->data[strlen(head->data) - 1] != 't' && \
-		   head->data[strlen(head->data) - 1] != 'f')
-	        head->data[strlen(head->data) - 1] = '\0';
+	    unsigned int data_len;
+	    if (head != NULL && head->data != NULL)
+		data_len = strlen(head->data);
+	    else
+		data_len = 0;
+
+	    if (data_len > 1)
+		{
+		char final_char_in_data = head->data[data_len - 1];
+	        while (!isdigit(head->data[data_len - 1]) && final_char_in_data != ')' && \
+		       final_char_in_data != 't' && final_char_in_data != 'f')	                 
+		    head->data[data_len - 1] = '\0';
+		}
 	    }
         else
             {
