998549d127c639ee9f266bafcb7133df2132f25c
WojciechZduniak09 <151409152+WojciechZduniak09@users.noreply.github.com>
2025-01-19 19:45:29 +0100

    idk if this stuff works

--- src/rpsecore-broadcast.c
+++ src/rpsecore-broadcast.c
@@ -548,7 +548,7 @@ rpse_broadcast_receiveBroadcast(const broadcast_data_t *BROADCAST_DATA)
             crypto_stream_chacha20_xor((unsigned char *)current_node->data, (const unsigned char *)current_broadcast_data->encrypted_message, 
                                                                 strlen(current_broadcast_data->encrypted_message) + 1,
                                                                 (const unsigned char *)current_broadcast_data->nonce, (const unsigned char *)BROADCAST_CHACHA20_ENCRYPTION_KEY);
-	    const char *INVALID_PATTERN = "^\\\\[0-9]+$";
+	    const char *INVALID_PATTERN = "^\\\\[1-9]+$";
 	    regex_t invalid_pattern_regex;
 	    if (regcomp(&invalid_pattern_regex, INVALID_PATTERN, REG_EXTENDED))
 	    	{
@@ -557,11 +557,18 @@ rpse_broadcast_receiveBroadcast(const broadcast_data_t *BROADCAST_DATA)
 		}
 	    if (current_node != NULL && current_node->data != NULL && strlen(current_node->data) > 1)
 		{
+		unsigned int attempts = 0;
 	        while (
-		       !regexec(&invalid_pattern_regex, current_node->data, 0, NULL, 0) || \
-		       current_node->data[strlen(current_node->data) - 1] == 'n' \
+		       (
+			!regexec(&invalid_pattern_regex, current_node->data, 0, NULL, 0) || \
+		        current_node->data[strlen(current_node->data) - 1] == 'n' || \
+		        current_node->data[strlen(current_node->data) - 1] != ')' \
+		       ) && attempts <= 3
 		      ) /* these just appear all the time */
-		      head->data[strlen(current_node->data) - 1] = '\0';
+	 	    {
+		    head->data[strlen(current_node->data) - 1] = '\0';
+		    attempts++;
+		    }
 		regfree(&invalid_pattern_regex);
 		}
 	    }
