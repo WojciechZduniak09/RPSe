225fe86f7326b029c48eff3534b8afc18c83b674
WojciechZduniak09 <151409152+WojciechZduniak09@users.noreply.github.com>
2025-01-19 19:24:20 +0100

    Dummy regex fix

--- src/rpsecore-broadcast.c
+++ src/rpsecore-broadcast.c
@@ -548,7 +548,7 @@ rpse_broadcast_receiveBroadcast(const broadcast_data_t *BROADCAST_DATA)
             crypto_stream_chacha20_xor((unsigned char *)current_node->data, (const unsigned char *)current_broadcast_data->encrypted_message, 
                                                                 strlen(current_broadcast_data->encrypted_message) + 1,
                                                                 (const unsigned char *)current_broadcast_data->nonce, (const unsigned char *)BROADCAST_CHACHA20_ENCRYPTION_KEY);
-	    const char *INVALID_PATTERN = "\\\\d";
+	    const char *INVALID_PATTERN = "^\d+$";
 	    regex_t invalid_pattern_regex;
 	    if (regcomp(&invalid_pattern_regex, INVALID_PATTERN, REG_EXTENDED))
 	    	{
